5cb7df25e2af56929571da463382c1ca734eacfc
Vivian Zhang <vivian.s.zhang@gmail.com>
2020-04-25 16:50:09 -0700

    Can star/add to meal plan from recipe

--- app/src/main/java/com/example/mealplanner/RecipeActivity.java
+++ app/src/main/java/com/example/mealplanner/RecipeActivity.java
@@ -1,35 +1,53 @@
 package com.example.mealplanner;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 public class RecipeActivity extends AppCompatActivity {
 
-    Button starBtn;
+    MenuItem starBtn;
     FirebaseUser user;
     FirebaseFirestore fStore;
     String recipeId, name, ingredients, instructions;
+    private AlertDialog alertDialog;
+    private String selected_day, selected_month, selected_year;
+    private SimpleDateFormat monthFormat = new SimpleDateFormat("MMMM",Locale.ENGLISH);
+    private SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy",Locale.ENGLISH);
+    private SimpleDateFormat mealsFormat = new SimpleDateFormat("yyyy-MM-dd",Locale.ENGLISH);
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -50,36 +68,9 @@ public class RecipeActivity extends AppCompatActivity {
         mIngredients.setText(ingredients);
         mInstructions.setText(instructions);
 
-        starBtn = findViewById(R.id.starRecipe_button);
         fStore = FirebaseFirestore.getInstance();
         user = FirebaseAuth.getInstance().getCurrentUser();
-        if(user != null){
-            DocumentReference documentReference = fStore.collection("users").document(user.getUid());
-            documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                @Override
-                public void onSuccess(DocumentSnapshot documentSnapshot) {
-                    List<String> recipe_ids = Arrays.asList(documentSnapshot.getString("starred").split(","));
-                    if(recipe_ids.contains(recipeId)){
-                        starBtn.setText("Remove from Starred Recipes");
-                    }else{
-                        starBtn.setText("Add to Starred Recipes");
-                    }
-                }
-            });
-        }else{
-            finish();
-            startActivity(new Intent(getApplicationContext(),Login.class));
-        }
-        starBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(starBtn.getText().charAt(0) == 'R'){
-                    removeStar();
-                }else{
-                    addStar();
-                }
-            }
-        });
+
     }
 
     private void removeStar(){
@@ -98,7 +89,7 @@ public class RecipeActivity extends AppCompatActivity {
                 documentReference.update("starred",new_ids).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        starBtn.setText("Add to Starred Recipes");
+                        starBtn.setTitle("Add Star");
                     }
                 });
             }
@@ -122,9 +113,106 @@ public class RecipeActivity extends AppCompatActivity {
                 documentReference.update("starred",new_ids).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        starBtn.setText("Remove from Starred Recipes");
+                        starBtn.setTitle("Remove Star");
                     }
                 });
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    public void showPopup(View v){
+        PopupMenu popupMenu = new PopupMenu(getApplicationContext(),v);
+        popupMenu.inflate(R.menu.recipe_menu);
+        Menu temp = popupMenu.getMenu();
+        starBtn = temp.findItem(R.id.starMenuItem);
+        if(user != null){
+            DocumentReference documentReference = fStore.collection("users").document(user.getUid());
+            documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    List<String> recipe_ids = Arrays.asList(documentSnapshot.getString("starred").split(","));
+                    if(recipe_ids.contains(recipeId)){
+                        starBtn.setTitle("Remove Star");
+                    }else{
+                        starBtn.setTitle("Add Star");
+                    }
+                }
+            });
+        }else{
+            finish();
+            startActivity(new Intent(getApplicationContext(),Login.class));
+        }
+        popupMenu.setForceShowIcon(true);
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.starMenuItem:
+                        if(starBtn.getTitle().charAt(0) == 'R'){
+                            removeStar();
+                        }else{
+                            addStar();
+                        }
+                        return true;
+                    case R.id.addMenuItem:
+                        AlertDialog.Builder builder = new AlertDialog.Builder(RecipeActivity.this);
+                        builder.setCancelable(true);
+                        final View addView = LayoutInflater.from(RecipeActivity.this).inflate(R.layout.recipe_add_meal,null);
+
+                        final EditText notes_et = addView.findViewById(R.id.notes);
+                        Button addMealBtn = addView.findViewById(R.id.add_meal_button);
+                        final DatePicker datePicker = addView.findViewById(R.id.datePicker);
+                        final Calendar today = Calendar.getInstance(Locale.ENGLISH);
+                        selected_day = mealsFormat.format(today.getTime());
+                        selected_month = monthFormat.format(today.getTime());
+                        selected_year = yearFormat.format(today.getTime());
+                        datePicker.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
+                            @Override
+                            public void onDateChanged(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                                Calendar c = Calendar.getInstance();
+                                c.set(year,month,dayOfMonth);
+                                selected_day = mealsFormat.format(c.getTime());
+                                selected_month = monthFormat.format(c.getTime());
+                                selected_year = yearFormat.format(c.getTime());
+                            }
+                        });
+                        addMealBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                                String notes = notes_et.getText().toString();
+                                saveMealPlan(recipeId,name,selected_day,selected_month,selected_year,notes);
+                                alertDialog.dismiss();
+                        }
+                        });
+
+                        builder.setView(addView);
+                        alertDialog = builder.create();
+                        alertDialog.show();
+
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+        popupMenu.show();
+    }
+
+    private void saveMealPlan(String id, String name, String day, String month, String year, String notes){
+        final FirebaseFirestore fStore = FirebaseFirestore.getInstance();
+        CollectionReference mealPlanCollection = fStore.collection("mealPlans");
+        Map<String,Object> data = new HashMap<>();
+        data.put("day",day);
+        data.put("month",month);
+        data.put("year",year);
+        data.put("notes",notes);
+        data.put("recipeId",id);
+        data.put("recipeName",name);
+        data.put("uid",user.getUid());
+        mealPlanCollection.add(data).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
             }
         });
     }
